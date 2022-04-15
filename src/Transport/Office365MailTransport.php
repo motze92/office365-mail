722867205608ed62faeac4bb6f041fd7caeaa454
Moritz Mair <mail@moritzmair.info>
2022-04-15 19:14:27 +0200

    laravel 9 support

--- src/Transport/Office365MailTransport.php
+++ src/Transport/Office365MailTransport.php
@@ -2,141 +2,138 @@
 
 namespace Office365Mail\Transport;
 
-use Illuminate\Mail\Transport\Transport;
-use Swift_Mime_SimpleMessage;
 use Microsoft\Graph\Graph;
 use Microsoft\Graph\Model\UploadSession;
+use Symfony\Component\Mailer\SentMessage;
+use Symfony\Component\Mailer\Transport\AbstractTransport;
+use Symfony\Component\Mime\MessageConverter;
 
-class Office365MailTransport extends Transport
+use Symfony\Component\Mime\Email;
+use Illuminate\Support\Str;
+
+class Office365MailTransport extends AbstractTransport
 {
 
     public function __construct()
     {
     }
 
-    public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
+    protected function doSend(SentMessage $message): void
+    // public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
 
-        $this->beforeSendPerformed($message);
+        // $this->beforeSendPerformed($message);
+        $email = MessageConverter::toEmail($message->getOriginalMessage());
 
         $graph = new Graph();
 
         $graph->setAccessToken($this->getAccessToken());
 
         // Special treatment if the message has too large attachments
-        $messageBody = $this->getBody($message, true);
+        $messageBody = $this->getBody($email, !!$email->getAttachments());
         $messageBodySizeMb = json_encode($messageBody);
         $messageBodySizeMb = strlen($messageBodySizeMb);
         $messageBodySizeMb = $messageBodySizeMb / 1048576; //byte -> mb
 
         if ($messageBodySizeMb >= 4) {
             unset($messageBody);
-            $graphMessage = $graph->createRequest("POST", "/users/" . key($message->getFrom()) . "/messages")
-                ->attachBody($this->getBody($message))
+            $graphMessage = $graph->createRequest("POST", "/users/" . $email->getFrom()[0]->getAddress() . "/messages")
+                ->attachBody($this->getBody($email))
                 ->setReturnType(\Microsoft\Graph\Model\Message::class)
                 ->execute();
 
-            foreach ($message->getChildren() as $attachment) {
-                if ($attachment instanceof \Swift_Mime_SimpleMimeEntity) {
-                    $fileName = $attachment->getHeaders()->get('Content-Disposition')->getParameter('filename');
-                    $content = $attachment->getBody();
-                    $fileSize = strlen($content);
-                    $size = $fileSize / 1048576; //byte -> mb
-                    $id = $attachment->getId();
-                    $attachmentMessage = [
-                        'AttachmentItem' => [
-                            'attachmentType' => 'file',
-                            'name' => $fileName,
-                            'size' => strlen($content)
-                        ]
+            foreach ($email->getAttachments() as $attachment) {
+                $fileName = $attachment->getPreparedHeaders()->getHeaderParameter('Content-Disposition', 'filename');
+                $content = $attachment->getBody();
+                $fileSize = strlen($content);
+                $size = $fileSize / 1048576; //byte -> mb
+                $id = Str::random(10);
+                $attachmentMessage = [
+                    'AttachmentItem' => [
+                        'attachmentType' => 'file',
+                        'name' => $fileName,
+                        'size' => strlen($content)
+                    ]
+                ];
+
+                if ($size <= 3) { //ErrorAttachmentSizeShouldNotBeLessThanMinimumSize if attachment <= 3mb, then we need to add this
+                    $attachmentBody = [
+                        "@odata.type" => "#microsoft.graph.fileAttachment",
+                        "name" => $attachment->getPreparedHeaders()->getHeaderParameter('Content-Disposition', 'filename'),
+                        "contentType" => $attachment->getPreparedHeaders()->get('Content-Type')->getValue(),
+                        "contentBytes" => base64_encode($attachment->getBody()),
+                        'contentId'    => $id
                     ];
 
-                    if ($size <= 3) { //ErrorAttachmentSizeShouldNotBeLessThanMinimumSize if attachment <= 3mb, then we need to add this
-                        $attachmentBody = [
-                            "@odata.type" => "#microsoft.graph.fileAttachment",
-                            "name" => $attachment->getHeaders()->get('Content-Disposition')->getParameter('filename'),
-                            "contentType" => $attachment->getBodyContentType(),
-                            "contentBytes" => base64_encode($attachment->getBody()),
-                            'contentId'    => $id
-                        ];
+                    $addAttachment = $graph->createRequest("POST", "/users/" . $email->getFrom()[0]->getAddress() . "/messages/" . $graphMessage->getId() . "/attachments")
+                        ->attachBody($attachmentBody)
+                        ->setReturnType(UploadSession::class)
+                        ->execute();
+                } else {
+                    //upload the files in chunks of 4mb....
+                    $uploadSession = $graph->createRequest("POST", "/users/" . $email->getFrom()[0]->getAddress() . "/messages/" . $graphMessage->getId() . "/attachments/createUploadSession")
+                        ->attachBody($attachmentMessage)
+                        ->setReturnType(UploadSession::class)
+                        ->execute();
 
-                        $addAttachment = $graph->createRequest("POST", "/users/" . key($message->getFrom()) . "/messages/" . $graphMessage->getId() . "/attachments")
-                            ->attachBody($attachmentBody)
-                            ->setReturnType(UploadSession::class)
-                            ->execute();
-                    } else {
-                        //upload the files in chunks of 4mb....
-                        $uploadSession = $graph->createRequest("POST", "/users/" . key($message->getFrom()) . "/messages/" . $graphMessage->getId() . "/attachments/createUploadSession")
-                            ->attachBody($attachmentMessage)
-                            ->setReturnType(UploadSession::class)
-                            ->execute();
+                    $fragSize =  1024 * 1024 * 4; //4mb at once...
+                    $numFragments = ceil($fileSize / $fragSize);
+                    $contentChunked = str_split($content, $fragSize);
+                    $bytesRemaining = $fileSize;
 
-                        $fragSize =  1024 * 1024 * 4; //4mb at once...
-                        $numFragments = ceil($fileSize / $fragSize);
-                        $contentChunked = str_split($content, $fragSize);
-                        $bytesRemaining = $fileSize;
-
-                        $i = 0;
-                        while ($i < $numFragments) {
-                            $chunkSize = $numBytes = $fragSize;
-                            $start = $i * $fragSize;
-                            $end = $i * $fragSize + $chunkSize - 1;
-                            if ($bytesRemaining < $chunkSize) {
-                                $chunkSize = $numBytes = $bytesRemaining;
-                                $end = $fileSize - 1;
-                            }
-                            $data = $contentChunked[$i];
-                            $content_range = "bytes " . $start . "-" . $end . "/" . $fileSize;
-                            $headers = [
-                                "Content-Length" => $numBytes,
-                                "Content-Range" => $content_range
-                            ];
-                            $client = new \GuzzleHttp\Client();
-                            $tmp = $client->put($uploadSession->getUploadUrl(), [
-                                'headers'         => $headers,
-                                'body'            => $data,
-                                'allow_redirects' => false,
-                                'timeout'         => 1000
-                            ]);
-                            $result = $tmp->getBody() . '';
-                            $result = json_decode($result); //if body == empty, then the file was successfully uploaded
-                            $bytesRemaining = $bytesRemaining - $chunkSize;
-                            $i++;
+                    $i = 0;
+                    while ($i < $numFragments) {
+                        $chunkSize = $numBytes = $fragSize;
+                        $start = $i * $fragSize;
+                        $end = $i * $fragSize + $chunkSize - 1;
+                        if ($bytesRemaining < $chunkSize) {
+                            $chunkSize = $numBytes = $bytesRemaining;
+                            $end = $fileSize - 1;
                         }
+                        $data = $contentChunked[$i];
+                        $content_range = "bytes " . $start . "-" . $end . "/" . $fileSize;
+                        $headers = [
+                            "Content-Length" => $numBytes,
+                            "Content-Range" => $content_range
+                        ];
+                        $client = new \GuzzleHttp\Client();
+                        $tmp = $client->put($uploadSession->getUploadUrl(), [
+                            'headers'         => $headers,
+                            'body'            => $data,
+                            'allow_redirects' => false,
+                            'timeout'         => 1000
+                        ]);
+                        $result = $tmp->getBody() . '';
+                        $result = json_decode($result); //if body == empty, then the file was successfully uploaded
+                        $bytesRemaining = $bytesRemaining - $chunkSize;
+                        $i++;
                     }
                 }
             }
 
             //definetly send the message
-            $graph->createRequest("POST", "/users/" . key($message->getFrom()) . "/messages/" . $graphMessage->getId() . "/send")->execute();
+            $graph->createRequest("POST", "/users/" . $email->getFrom()[0]->getAddress() . "/messages/" . $graphMessage->getId() . "/send")->execute();
         } else {
-            $graphMessage = $graph->createRequest("POST", "/users/" . key($message->getFrom()) . "/sendmail")
+            $graphMessage = $graph->createRequest("POST", "/users/" . $email->getFrom()[0]->getAddress() . "/sendmail")
                 ->attachBody($messageBody)
                 ->setReturnType(\Microsoft\Graph\Model\Message::class)
                 ->execute();
         }
-
-        $this->sendPerformed($message);
-
-        return $this->numberOfRecipients($message);
     }
 
     /**
      * Get body for the message.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @param bool $withAttachments
      * @return array
      */
 
-    protected function getBody(Swift_Mime_SimpleMessage $message, $withAttachments = false)
+    protected function getBody(Email $message, $withAttachments = false)
     {
         $messageData = [
             'from' => [
-                'emailAddress' => [
-                    'address' => key($message->getFrom()),
-                    'name' => current($message->getFrom())
-                ]
+                'emailAddress' => $message->getFrom()[0]
             ],
             'toRecipients' => $this->getTo($message),
             'ccRecipients' => $this->getCc($message),
@@ -144,25 +141,24 @@ class Office365MailTransport extends Transport
             'replyTo' => $this->getReplyTo($message),
             'subject' => $message->getSubject(),
             'body' => [
-                'contentType' => $message->getBodyContentType() == "text/html" ? 'html' : 'text',
-                'content' => $message->getBody()
+                'contentType' => $message->getHtmlBody() ? 'html' : 'text',
+                'content' => $message->getHtmlBody() ? $message->getHtmlBody() : $message->getTextBody()
             ]
         ];
+        $messageData = ['message' => $messageData];
 
         if ($withAttachments) {
-            $messageData = ['message' => $messageData];
             //add attachments if any
             $attachments = [];
-            foreach ($message->getChildren() as $attachment) {
-                if ($attachment instanceof \Swift_Mime_SimpleMimeEntity) {
-                    $attachments[] = [
-                        "@odata.type" => "#microsoft.graph.fileAttachment",
-                        "name" => $attachment->getHeaders()->get('Content-Disposition')->getParameter('filename'),
-                        "contentType" => $attachment->getBodyContentType(),
-                        "contentBytes" => base64_encode($attachment->getBody()),
-                        'contentId'    => $attachment->getId()
-                    ];
-                }
+            foreach ($message->getAttachments() as $attachment) {
+                $headers = $attachment->getPreparedHeaders();
+                $attachments[] = [
+                    "@odata.type" => "#microsoft.graph.fileAttachment",
+                    "name" => $headers->getHeaderParameter('Content-Disposition', 'filename'),
+                    "contentType" => $headers->get('Content-Type')->getValue(),
+                    "contentBytes" => base64_encode($attachment->getBody()),
+                    'contentId'    => Str::random(10)
+                ];
             }
             if (count($attachments) > 0) {
                 $messageData['message']['attachments'] = $attachments;
@@ -175,20 +171,20 @@ class Office365MailTransport extends Transport
     /**
      * Get the "to" payload field for the API request.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @return string
      */
-    protected function getTo(Swift_Mime_SimpleMessage $message)
+    protected function getTo(Email $message)
     {
-        return collect((array) $message->getTo())->map(function ($display, $address) {
-            return $display ? [
+        return collect((array) $message->getTo())->map(function ($address) {
+            return $address->getName() ? [
                 'emailAddress' => [
-                    'address' => $address,
-                    'name' => $display
+                    'address' => $address->getAddress(),
+                    'name' => $address->getName()
                 ]
             ] : [
                 'emailAddress' => [
-                    'address' => $address
+                    'address' => $address->getAddress()
                 ]
             ];
         })->values()->toArray();
@@ -197,20 +193,20 @@ class Office365MailTransport extends Transport
     /**
      * Get the "Cc" payload field for the API request.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @return string
      */
-    protected function getCc(Swift_Mime_SimpleMessage $message)
+    protected function getCc(Email $message)
     {
-        return collect((array) $message->getCc())->map(function ($display, $address) {
-            return $display ? [
+        return collect((array) $message->getCc())->map(function ($address) {
+            return $address->getName() ? [
                 'emailAddress' => [
-                    'address' => $address,
-                    'name' => $display
+                    'address' => $address->getAddress(),
+                    'name' => $address->getName()
                 ]
             ] : [
                 'emailAddress' => [
-                    'address' => $address
+                    'address' => $address->getAddress()
                 ]
             ];
         })->values()->toArray();
@@ -219,20 +215,20 @@ class Office365MailTransport extends Transport
     /**
      * Get the "replyTo" payload field for the API request.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @return string
      */
-    protected function getReplyTo(Swift_Mime_SimpleMessage $message)
+    protected function getReplyTo(Email $message)
     {
-        return collect((array) $message->getReplyTo())->map(function ($display, $address) {
-            return $display ? [
+        return collect((array) $message->getReplyTo())->map(function ($address) {
+            return $address->getName() ? [
                 'emailAddress' => [
-                    'address' => $address,
-                    'name' => $display
+                    'address' => $address->getAddress(),
+                    'name' => $address->getName()
                 ]
             ] : [
                 'emailAddress' => [
-                    'address' => $address
+                    'address' => $address->getAddress()
                 ]
             ];
         })->values()->toArray();
@@ -241,10 +237,10 @@ class Office365MailTransport extends Transport
     /**
      * Get the "Bcc" payload field for the API request.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @return string
      */
-    protected function getBcc(Swift_Mime_SimpleMessage $message)
+    protected function getBcc(Email $message)
     {
         return collect((array) $message->getBcc())->map(function ($display, $address) {
             return $display ? [
@@ -263,10 +259,10 @@ class Office365MailTransport extends Transport
     /**
      * Get all of the contacts for the message.
      *
-     * @param \Swift_Mime_SimpleMessage $message
+     * @param Symfony\Component\Mime\Email $message
      * @return array
      */
-    protected function allContacts(Swift_Mime_SimpleMessage $message)
+    protected function allContacts(Email $message)
     {
         return array_merge(
             (array) $message->getTo(),
@@ -290,5 +286,15 @@ class Office365MailTransport extends Transport
         ])->getBody()->getContents());
 
         return $token->access_token;
+    }
+
+    /**
+     * Get the string representation of the transport.
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return 'office365mail';
     }
 }
